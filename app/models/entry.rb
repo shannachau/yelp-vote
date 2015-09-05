4685d3c3661cc530ba681b565965b7f6d1017307
Shanna Chau <shannachau@gmail.com>
2015-09-04 19:46:01 -0700

    Adding associations and validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,9 @@
 class Entry < ActiveRecord::Base
+  # Validations
+  validates :user_id, presence: true
+  validates :message_id, presence: true
+
+  # Associations
+  belongs_to :user
+  belongs_to :message
 end
