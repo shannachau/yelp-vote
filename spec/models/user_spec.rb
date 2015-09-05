197560eddb78b372d77c8cf6cf75e55517965e36
Shanna Chau <shannachau@gmail.com>
2015-09-04 20:39:53 -0700

    WIP

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "creates a valid user" do
+      user = build(:user)
+
+      expect(user).to be_valid
+    end
+
+    it "requires a username" do
+
+    end
+  end
 end
