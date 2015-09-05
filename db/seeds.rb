35add1933d13646935e1d0410c0d430faf26f357
Shanna Chau <shannachau@gmail.com>
2015-09-04 19:48:38 -0700

    Adding seeds for creating messages and entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,13 @@
 User.create(username: "shanna", password: "shanna", password_confirmation: "shanna")
 User.create(username: "pitbull", password: "pitbull", password_confirmation: "pitbull")
 User.create(username: "seanpaul", password: "seanpaul", password_confirmation: "seanpaul")
+
+Message.create
+Message.create
+
+
+Entry.create(text: "Hey dude", user_id: 1, message_id: 1)
+Entry.create(text: "What's up", user_id: 2, message_id: 1)
+Entry.create(text: "Let's go out tonight!", user_id: 3, message_id: 2)
+Entry.create(text: "Okay sounds good", user_id: 2, message_id: 2)
+Entry.create(text: "Where do you wanna go?", user_id: 1, message_id: 2)
