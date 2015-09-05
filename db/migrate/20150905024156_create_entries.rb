4c3795235d53df6b9ccffa9fa68e921a44152ec0
Shanna Chau <shannachau@gmail.com>
2015-09-04 19:46:11 -0700

    Generate migration for Entry model

--- db/migrate/20150905024156_create_entries.rb
+++ db/migrate/20150905024156_create_entries.rb
@@ -1,7 +1,9 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
-
+      t.string :text
+      t.integer :user_id, null: false
+      t.integer :message_id, null: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905023446) do
+ActiveRecord::Schema.define(version: 20150905024156) do
+
+  create_table "entries", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "user_id",    null: false
+    t.integer  "message_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "message_user", id: false, force: :cascade do |t|
     t.integer "user_id",    null: false
