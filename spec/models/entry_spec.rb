94a76a41be91f12a746f01d89eedc29c2d675989
Shanna Chau <shannachau@gmail.com>
2015-09-04 20:26:57 -0700

    Adding tests for validations and associations

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "creates a valid model" do
+      entry = build(:entry)
+
+      expect(entry).to be_valid
+    end
+
+    it "requires a user_id" do
+      entry = build(:entry, user_id: nil)
+
+      expect(entry).to be_invalid
+      expect(entry.errors).to include(:user_id)
+    end
+
+    it "requires a message_id" do
+      entry = build(:entry, message_id: nil)
+
+      expect(entry).to be_invalid
+      expect(entry.errors).to include(:message_id)
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a user" do
+      user = create(:user)
+      entry = create(:entry, user_id: user.id)
+
+      expect(entry.user).to eq(user)
+    end
+
+    it "belongs to a message" do
+      message = create(:message)
+      entry = create(:entry, message_id: message.id)
+
+      expect(entry.message).to eq(message)
+    end
+  end
 end
