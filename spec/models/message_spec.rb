f8e812a6f645790debe46439c88476b7f830e58f
Shanna Chau <shannachau@gmail.com>
2015-09-04 20:36:46 -0700

    Adding tests for associations for message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    before do
+      @message = create(:message)
+      @user1 = create(:user, username: "user1")
+      @user2 = create(:user, username: "user2")
+
+      @message.users << @user1
+      @message.users << @user2
+    end
+    it "has many many users" do
+      expect(@message.users).to eq([@user1, @user2])
+    end
+
+    it "belongs to many users" do
+      expect(@user1.messages).to include(
+      @message)
+      expect(@user2.messages).to include(@message)
+    end
+  end
 end
