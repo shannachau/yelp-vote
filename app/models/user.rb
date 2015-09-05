0a2fed78bb0e4812ac9e03539f7957d05a4fc04d
Shanna Chau <shannachau@gmail.com>
2015-09-04 19:20:54 -0700

    Adding validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # Validations
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  # Associations
+  has_secure_password
+
+
 end
