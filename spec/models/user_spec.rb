84066f27d06a1504d60e00fc4d41741de36fb32a
Shanna Chau <shannachau@gmail.com>
2015-09-06 15:00:20 -0700

    Added tests for validations and associations for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,59 @@ RSpec.describe User, type: :model do
     end
 
     it "requires a username" do
+      user = build(:user, username: nil)
 
+      expect(user).to be_invalid
+      expect(user.errors).to include(:username)
+    end
+
+    it "requires a unique username" do
+      user1 = create(:user, username: "username")
+      user2 = build(:user, username: "username")
+
+      expect(user2).to be_invalid
+      expect(user2.errors).to include(:username)
+    end
+
+    it "requires a password" do
+      user = build(:user, password: nil)
+
+      expect(user).to be_invalid
+      expect(user.errors).to include(:password)
+    end
+
+    it "requires a password_confirmation" do
+      user = build(:user, password_confirmation: nil)
+
+      expect(user).to be_invalid
+      expect(user.errors).to include(:password_confirmation)
+    end
+  end
+
+  describe "assocations" do
+    before do
+      @user = create(:user)
+      @message1 = create(:message)
+      @message2 = create(:message)
+
+      @user.messages << @message1
+      @user.messages << @message2
+
+      @entry1 = create(:entry, user_id: @user.id)
+      @entry2 = create(:entry, user_id: @user.id)
+    end
+
+    it "has many messages" do
+      expect(@user.messages).to eq([@message1, @message2])
+    end
+
+    it "belongs to many messages" do
+      expect(@message1.users).to include(@user)
+      expect(@message2.users).to include(@user)
+    end
+
+    it "has many entries" do
+      expect(@user.entries).to eq([@entry1, @entry2])
     end
   end
 end
