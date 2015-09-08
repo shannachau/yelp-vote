ede7fc75906347ca20aaf2678a09bc08a4a9dfda
Shanna Chau <shannachau@gmail.com>
2015-09-08 14:47:44 -0700

    Adding login/logout functionality in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+    end
+
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get '/login' => 'sessions#new', as: 'login'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', as: 'logout'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
