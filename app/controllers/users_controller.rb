8df52a0c6934663c5733aa262bc12e5e098b481a
Shanna Chau <shannachau@gmail.com>
2015-09-08 15:27:32 -0700

    Adding create function in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(create_params)
 
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "Try again!"
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
 end
