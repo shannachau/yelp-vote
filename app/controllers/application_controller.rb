32b178bf3cfb31211aa884077aee4bbdc1911af6
Shanna Chau <shannachau@gmail.com>
2015-09-09 00:19:08 -0700

    Adding function for require_login filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def require_login
+    unless session[:user_id]
+      flash[:error] = "You must be logged in to perform this action."
+    end
+    redirect_to login_path
+  end
 end
