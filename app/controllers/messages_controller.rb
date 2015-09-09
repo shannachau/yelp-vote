c57dcdcd381303882f5f3003750a7a0f47ecf359
Shanna Chau <shannachau@gmail.com>
2015-09-08 19:22:18 -0700

    Index and show methods. scaffolding for require login before action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,14 @@
 class MessagesController < ApplicationController
+  def index
+    user = User.find_by(id: session[:user_id])
+    @messages = user.messages
+  end
+
+  def show
+  end
+
+  private
+
+  def require_login
+  end
 end
