6403f5b14cf03bbacb0ffa67d1754366ee9efe44
Shanna Chau <shannachau@gmail.com>
2015-09-08 18:33:38 -0700

    Adding route for messages index

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/users/new' => 'users#new'
   post '/users/new' => 'users#create'
 
+  get '/messages' => 'messages#index'
 
 
   # Example of regular route:
