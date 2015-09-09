e780cdc2ca552b057cb86d28e6ec0b233003ae5d
Shanna Chau <shannachau@gmail.com>
2015-09-09 00:19:45 -0700

    Adding before_action require_login

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,6 @@
 class EntriesController < ApplicationController
+  before_action :require_login
+
   def create
     entry = Entry.create(create_params)
     redirect_to message_path(entry.message_id)
