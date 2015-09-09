6056217264029423e7df920ab780203bb95e2c8d
Shanna Chau <shannachau@gmail.com>
2015-09-08 19:58:35 -0700

    User can add a new entry to a message

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,12 @@
 class EntriesController < ApplicationController
+  def create
+    entry = Entry.create(create_params)
+    redirect_to message_path(entry.message_id)
+  end
+
+  private
+
+  def create_params
+    params.require(:entry).permit(:text, :user_id, :message_id)
+  end
 end
