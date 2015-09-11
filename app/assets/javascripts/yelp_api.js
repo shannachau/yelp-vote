8392f85c883abdb046fca32aedcb29fa6b1a8bb2
Shanna Chau <shannachau@gmail.com>
2015-09-10 18:04:44 -0700

    Adding some divs for styling search results

--- app/assets/javascripts/yelp_api.js
+++ app/assets/javascripts/yelp_api.js
@@ -25,18 +25,62 @@ $(function() {
     $('.results').empty();
 
     for (var i = 0; i < data.length; i++) {
+      var listItem = $('<li></li>');
+      listItem.addClass("list-group-item");
+
+      //row
+      var rowResult = $("<div class='row'></div>");
+
+      //column
+      var imgColumn = $("<div class='col-md-4'></div>");
+
+      var img = $('<img>');
+      img.attr('src', data[i].image_url);
+      imgColumn.append(img);
+
+
+      //column
+      var infoColumn = $("<div class='col-md-8'></div>");
+
       var anchor = $('<a></a>');
       anchor.text(data[i].name);
       anchor.prop('href', data[i].url);
+      infoColumn.append(anchor);
 
-      $('.results').append(anchor);
-      anchor.wrap('<li></li>');
+      var rating_img = $('<img>');
+      rating_img.addClass("rating");
+      rating_img.attr('src', data[i].rating_image);
+      infoColumn.append(rating_img);
+
+      var location = data[i].location;
+
+      for (var j = 0; j < location.length; j++){
+        var address = $('<p></p>');
+        address.text(data[i].location[j]);
+        infoColumn.append(address);
+      }
+
+      rowResult.append(imgColumn);
+      rowResult.append(infoColumn);
+
+      listItem.append(rowResult);
+
+      $('.results').append(listItem);
     }
   }
 });
 
+// ul
+//   li
+//     image   restaurant name as a link
+//             rating image    number of reviews
+//             location
+//   li
+// ul
+
 // name: business.name,
 // url: business.url,
+
 // image_url: business.image_url,
 // rating: business.rating,
 // rating_image: business.rating_img_url_small,
