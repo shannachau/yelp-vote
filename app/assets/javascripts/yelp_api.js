5ef1205441a0da966d0bc1817bb6372aeefb2112
Shanna Chau <shannachau@gmail.com>
2015-09-11 17:11:03 -0700

    Commenting out javascript for now

--- app/assets/javascripts/yelp_api.js
+++ app/assets/javascripts/yelp_api.js
@@ -2,73 +2,120 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(function() {
-  $('.search').click(function() {
-    event.preventDefault();
-
-    var form = $(this).parent();
-    var url = form.attr('action');
-    var method = form.attr('method');
-
-    $.ajax(url, {
-      type: method,
-      data: { 'search_term' : $('#search_term').val(),
-              'location' : $('#location').val()
-            },
-      success: function (data) {
-        formatData(data);
-      }
-    });
-  });
-
-  function formatData(data) {
-    $('.results').empty();
-
-    for (var i = 0; i < data.length; i++) {
-      var listItem = $('<li></li>');
-      listItem.addClass("list-group-item");
-
-      //row
-      var rowResult = $("<div class='row'></div>");
-
-      //column
-      var imgColumn = $("<div class='col-md-4'></div>");
-
-      var img = $('<img>');
-      img.attr('src', data[i].image_url);
-      imgColumn.append(img);
-
-
-      //column
-      var infoColumn = $("<div class='col-md-8'></div>");
-
-      var anchor = $('<a></a>');
-      anchor.text(data[i].name);
-      anchor.prop('href', data[i].url);
-      infoColumn.append(anchor);
-
-      var rating_img = $('<img>');
-      rating_img.addClass("rating");
-      rating_img.attr('src', data[i].rating_image);
-      infoColumn.append(rating_img);
-
-      var location = data[i].location;
-
-      for (var j = 0; j < location.length; j++){
-        var address = $('<p></p>');
-        address.text(data[i].location[j]);
-        infoColumn.append(address);
-      }
-
-      rowResult.append(imgColumn);
-      rowResult.append(infoColumn);
-
-      listItem.append(rowResult);
-
-      $('.results').append(listItem);
-    }
-  }
-});
+// $(function() {
+//   $('.search').click(function() {
+//     event.preventDefault();
+//
+//     var form = $(this).parent();
+//     var url = form.attr('action');
+//     var method = form.attr('method');
+//
+//     $.ajax(url, {
+//       type: method,
+//       data: { 'search_term' : $('#search_term').val(),
+//               'location' : $('#location').val()
+//             },
+//       success: function (data) {
+//         formatData(data);
+//       }
+//     });
+//   });
+//
+//   $('.search').click(function() {
+//     event.preventDefault();
+//
+//     var form = $(this).parent();
+//     var url = form.attr('action');
+//     var method = form.attr('method');
+//
+//     $.ajax(url, {
+//       type: method,
+//       data: { 'search_term' : $('#search_term').val(),
+//               'location' : $('#location').val()
+//             },
+//       success: function (data) {
+//         formatData(data);
+//       }
+//     });
+//   });
+//
+//
+//     $('.add-suggestion').click(function() {
+//       var url = window.location.pathname + "/entries";
+//       var method = "post";
+//
+//
+//       // pathname = window.location.pathname;
+//       // index = pathname.lastIndexOf("/");
+//       // message_id = pathname.slice((index + 1), pathname.length);
+//
+//       $.ajax(url, {
+//         type: method,
+//         data: { user_id: ,
+//                 message_id: ,
+//                 yelp_suggestion: true
+//               },
+//         success: function (data) {
+//           formatData(data);
+//         }
+//       });
+//     });
+//
+//   function formatData(data) {
+//     $('.results').empty();
+//
+//     for (var i = 0; i < data.length; i++) {
+//       var listItem = $('<li></li>');
+//       listItem.addClass("list-group-item");
+//
+//       //row
+//       var rowResult = $("<div class='row'></div>");
+//
+//       //column
+//       var imgColumn = $("<div class='col-md-3'></div>");
+//
+//       var img = $('<img>');
+//       img.attr('src', data[i].image_url);
+//       imgColumn.append(img);
+//
+//
+//       //column
+//       var infoColumn = $("<div class='col-md-5'></div>");
+//
+//       var anchor = $('<a></a>');
+//       anchor.text(data[i].name);
+//       anchor.prop('href', data[i].url);
+//       infoColumn.append(anchor);
+//
+//       var rating_img = $('<img>');
+//       rating_img.addClass("rating");
+//       rating_img.attr('src', data[i].rating_image);
+//       infoColumn.append(rating_img);
+//
+//       var location = data[i].location;
+//
+//       for (var j = 0; j < location.length; j++){
+//         var address = $('<p></p>');
+//         address.text(data[i].location[j]);
+//         infoColumn.append(address);
+//       }
+//
+//       //column (form)
+//       var buttonColumn = $("<div class='col-md-3'></div>");
+//       var submit = $("<input type='submit' value='Add Suggestion' class='btn btn-xs btn-primary add-suggestion'>");
+//
+//       buttonColumn.append(submit);
+//
+//       rowResult.append(imgColumn);
+//       rowResult.append(infoColumn);
+//       rowResult.append(buttonColumn);
+//
+//       listItem.append(rowResult);
+//
+//       $('.results').append(listItem);
+//     }
+//   }
+// });
 
 // ul
 //   li
